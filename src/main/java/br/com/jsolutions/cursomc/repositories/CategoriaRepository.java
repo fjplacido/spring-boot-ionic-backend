79b5fc202532e86428d08a26de90fc8660425ca2
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2025-09-12 22:35:58 -0300

    Criando repository e service para Catetoria

---
+++ src/main/java/br/com/jsolutions/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.com.jsolutions.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jsolutions.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Serializable> {
+
+}
---
+++ src/main/java/br/com/jsolutions/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package br.com.jsolutions.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jsolutions.cursomc.domain.Categoria;
+import br.com.jsolutions.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	public CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
