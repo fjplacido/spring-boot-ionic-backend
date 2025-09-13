a8c41051923b2a37cd90ee6d4747d464fa45ed44
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2025-09-12 23:09:47 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/br/com/jsolutions/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.jsolutions.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jsolutions.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Serializable> {
+
+}
