9898f57c599b6bfb519b3f351fe0560ba18aa588
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2025-09-20 11:49:29 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/jsolutions/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.jsolutions.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jsolutions.cursomc.domain.Pedido;
+import br.com.jsolutions.cursomc.repositories.PedidoRepository;
+import br.com.jsolutions.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	public PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
