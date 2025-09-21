344c0d263e983081a367817b9d3d3c82195d9723
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2025-09-20 22:05:49 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/br/com/jsolutions/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/jsolutions/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -54,6 +56,16 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(p -> new CategoriaDTO(p)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer LinesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, LinesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDTO = list.map(p -> new CategoriaDTO(p));
 		return ResponseEntity.ok().body(listDTO);
 	}
 
--- src/test/java/br/com/jsolutions/cursomc/CursomcApplicationTests.java
+++ src/test/java/br/com/jsolutions/cursomc/CursomcApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.jsolutions.cursomc;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CursomcApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
